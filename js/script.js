c3a605409c7b205e62865837417e0491239f1deb
lil_crash <lilcrash1006@gmail.com>
2022-01-26 17:41:47 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+console.log('add' + "- object");
+console.log('3' + "- object");
+
+let incr = 10,
+    decr = 10;
+
+//++incr;
+//--decr;    
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(747%2);
+
+console.log(2*4 == 8);
+
+ 
